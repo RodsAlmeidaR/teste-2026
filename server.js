a378a4cbb446d409cbb8ab6925e372cb94416eec
Rodrigo <rodsalmeidar@gmail.com>
2026-01-10 17:22:43 -0300

    feat: implementa formulario e rota de cadastro de colaboradores

--- server.js
+++ server.js
@@ -28,6 +28,34 @@ app.get('/', (req, res) => {
     });
 });
 
+//rota abrir formulario 
+app.get('/adicionar', (req, res) => {
+    res.render('adicionar');
+});
+
+//rota recebe dados e salva
+app.post('/salvar', (req, res) => {
+    const { nome, re, cargo, salario_atual, empresa } = req.body;
+
+    //defini valores padrão
+    const status = 'Ativo';
+    const salario_anterior = 0; //cadastro novo sem historico
+    
+    //comando sql preparo
+    const sql = `INSERT INTO colaboradores (nome, re, cargo, salario_atual, salario_anterior, status, empresa) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+
+    //executa no banco
+    db.run(sql, [nome, re, cargo, salario_atual, salario_anterior, status, empresa], (err) => {
+        if (err) {
+            console.error("Erro ao salvar:", err.message);
+            return res.send("Erro ao salvar colaborador: " + err.message);
+        }
+        //sucesso volta para a lista principal
+        console.log(`Colaborador ${nome} cadastrado com sucesso.`);
+        res.redirect('/');
+    });
+});
+
 //incia o servidor
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
